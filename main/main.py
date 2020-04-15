8c0377326bdf4bb27d7371c611f4ab2e0e188ee6
bg823 <nagaoka0132@gmail.com>
2020-04-15 23:03:43 +0900

    show gray image bg823.png .

--- main/main.py
+++ main/main.py
@@ -1,5 +1,12 @@
 #import OpenCV
+import numpy as np
 import cv2
 
-#Check version
-print(cv2.__version__)
+img = cv2.imread('images/bg823.png', 0)
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.imshow('image', img)
+
+while True:
+    if cv2.waitKey(0) & 0xFF == 27:
+        cv2.destroyAllWindows()
+        break
