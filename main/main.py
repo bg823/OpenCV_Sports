df88922bc50a20d02a603a7326b007afb2db4495
bg823 <nagaoka0132@gmail.com>
2020-04-15 22:48:38 +0900

    create three color images and save.(red, blue, green)

--- main/main.py
+++ main/main.py
@@ -1,5 +1,36 @@
-#import OpenCV
-import cv2
+# create 3images, blue/red/green
 
-#Check version
-print(cv2.__version__)
+import numpy as np  # import numpy
+import cv2  # import opencv
+
+# initialize with 0.
+# create Three-dimensional array is type uint8
+img = np.zeros((400, 400, 3), np.uint8)
+
+# create blue image
+img[:, :] = [255, 0, 0]
+# writing blue image in main folder
+cv2.imwrite('/your_folder/blueImage.jpg', img)
+# show image on window
+cv2.imshow('img1', img)
+
+# create green image
+img[:, :] = [0, 255, 0]
+# writing blue image in main folder
+cv2.imwrite('/your_folder/greenImage.jpg', img)
+# show image on window
+cv2.imshow('img2', img)
+
+# create red image
+img[:, :] = [0, 0, 255]
+# writing blue image in main folder
+cv2.imwrite('/your_folder/redImage.jpg', img)
+# show image on window
+cv2.imshow('img3', img)
+
+while True:
+    # push esc kye is exit
+    if cv2.waitKey(0) & 0xff == 27:
+        # closing all Window
+        cv2.destroyAllWindows()
+        break
