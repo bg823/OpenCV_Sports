b43b5229dd25a1ffa14e39dced1865f0841fcfb2
bg823 <nagaoka0132@gmail.com>
2020-04-17 02:43:27 +0900

    add comment and rewrote save image file path.

--- main/main.py
+++ main/main.py
@@ -5,26 +5,28 @@ import cv2  # import opencv
 
 # initialize with 0.
 # create Three-dimensional array is type uint8
+# image size 400 * 400 and three colors(channel)
+# np.zeroz((line, row, color_channel))
 img = np.zeros((400, 400, 3), np.uint8)
 
 # create blue image
 img[:, :] = [255, 0, 0]
 # writing blue image in main folder
-cv2.imwrite('/your_folder/blueImage.jpg', img)
+cv2.imwrite('result/blueImage.jpg', img)
 # show image on window
 cv2.imshow('img1', img)
 
 # create green image
 img[:, :] = [0, 255, 0]
 # writing blue image in main folder
-cv2.imwrite('/your_folder/greenImage.jpg', img)
+cv2.imwrite('result/greenImage.jpg', img)
 # show image on window
 cv2.imshow('img2', img)
 
 # create red image
 img[:, :] = [0, 0, 255]
 # writing blue image in main folder
-cv2.imwrite('/your_folder/redImage.jpg', img)
+cv2.imwrite('result/redImage.jpg', img)
 # show image on window
 cv2.imshow('img3', img)
 
