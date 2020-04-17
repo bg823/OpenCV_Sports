af0dc4649cb76a0232b7b254c0bbfa7007b1d86d
bg823 <nagaoka0132@gmail.com>
2020-04-18 02:12:06 +0900

    use OpenCV for create histgram.

--- main/main.py
+++ main/main.py
@@ -1,18 +1,29 @@
 import cv2
+# ヒストグラム表示に使う
+from matplotlib import pyplot as plt
 
-# 今回はcandyの画像をグレーで読みこむ
+# 今回はcandyの画像をカラーで読みこむ
 # 画像が作業ディレクトリにある場合は、そのパスを、他のディレクトリにある場合は、フルパスをい入れる必要があります
 # 1でカラー, 0でグレー, -1でアルファチャネルを含む画像を読み込みます
-img = cv2.imread('/Users/h-nagaoka/Dev/OpenCV_Sports/images/candy.jpg', 0)
+img = cv2.imread('/Users/h-nagaoka/Dev/OpenCV_Sports/images/candy.jpg', 1)
 
-# 作成したグレー画像を保存する(好きなところへ書き出してください)
-cv2.imwrite('result/gray_candy.jpg', img)
+# 塗り潰しで表示
+plt.hist(img.ravel(), 256, [0, 256])
+#
+plt.figure()
 
-# candy_imgというWindow名で読み込んだ画像を表示
-cv2.imshow('candy_img', img)
 
-# 何かしらのキーボードのキーが押されるまで、Windowを表示し続ける
-cv2.waitKey(0)
+# colorのタプル
+color = ('b', 'g', 'r')
 
-# キーが押されたら、Windowを破棄する
-cv2.destroyAllWindows()
+hist = 0
+for i, col in enumerate(color):
+    # colorのインデックスとbgrを格納
+    histr = cv2.calcHist([img], [i], None, [256], [0, 256])
+    # ヒストグラムをプロット
+    plt.plot(histr, color=col)
+    # X軸を0-256の間でヒストグラム表を作る
+    plt.xlim([0, 256])
+
+# プロットの表示
+plt.show()
