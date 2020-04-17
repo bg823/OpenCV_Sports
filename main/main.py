009860e5ddd49d8e3dec7241050ce67a8e9f4e87
bg823 <nagaoka0132@gmail.com>
2020-04-18 06:10:14 +0900

    auto save plot images.

--- main/main.py
+++ main/main.py
@@ -5,25 +5,22 @@ from matplotlib import pyplot as plt
 # 今回はcandyの画像をカラーで読みこむ
 # 画像が作業ディレクトリにある場合は、そのパスを、他のディレクトリにある場合は、フルパスをい入れる必要があります
 # 1でカラー, 0でグレー, -1でアルファチャネルを含む画像を読み込みます
-img = cv2.imread('/Users/h-nagaoka/Dev/OpenCV_Sports/images/candy.jpg', 1)
+img = cv2.imread('/your file path/gray_candy.jpg', 0)
 
 # 塗り潰しで表示
 plt.hist(img.ravel(), 256, [0, 256])
-#
+# プロットの保存
+plt.savefig('result/gray_img_1.png')
+
+# gray画像をヒストグラム表示させるために、計算
+# 実線で表示
+gray_img_2 = cv2.calcHist([img], [0], None, [256], [0, 256])
+# 新しく図を作成
 plt.figure()
-
-
-# colorのタプル
-color = ('b', 'g', 'r')
-
-hist = 0
-for i, col in enumerate(color):
-    # colorのインデックスとbgrを格納
-    histr = cv2.calcHist([img], [i], None, [256], [0, 256])
-    # ヒストグラムをプロット
-    plt.plot(histr, color=col)
-    # X軸を0-256の間でヒストグラム表を作る
-    plt.xlim([0, 256])
+# プロット作成
+plt.plot(gray_img_2)
+# プロットの保存
+plt.savefig('result/gray_img_2.png')
 
 # プロットの表示
 plt.show()
