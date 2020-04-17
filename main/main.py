f613d4f347e87a319b9305b78d18b44e74bdd8e7
bg823 <nagaoka0132@gmail.com>
2020-04-17 21:21:09 +0900

    グレーイメージで画像を読み込んで、書き出しと表示を行う.

--- main/main.py
+++ main/main.py
@@ -1,9 +1,12 @@
 import cv2
 
-# 今回はcandyの画像をカラーで読みこむ
+# 今回はcandyの画像をグレーで読みこむ
 # 画像が作業ディレクトリにある場合は、そのパスを、他のディレクトリにある場合は、フルパスをい入れる必要があります
 # 1でカラー, 0でグレー, -1でアルファチャネルを含む画像を読み込みます
-img = cv2.imread('your file dir/candy.jpg', 1)
+img = cv2.imread('/Users/h-nagaoka/Dev/OpenCV_Sports/images/candy.jpg', 0)
+
+# 作成したグレー画像を保存する(好きなところへ書き出してください)
+cv2.imwrite('result/gray_candy.jpg', img)
 
 # candy_imgというWindow名で読み込んだ画像を表示
 cv2.imshow('candy_img', img)
