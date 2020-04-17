e56992c1d01a9f029a67bbfc9f8668bde5a21088
bg823 <nagaoka0132@gmail.com>
2020-04-18 06:20:02 +0900

    before Conflict resolution I accidentally pushed the commit.

--- main/main.py
+++ main/main.py
@@ -12,7 +12,6 @@ plt.hist(img.ravel(), 256, [0, 256])
 # プロットの保存
 plt.savefig('result/gray_img_1.png')
 
-<<<<<<< HEAD
 # gray画像をヒストグラム表示させるために、計算
 # 実線で表示
 gray_img_2 = cv2.calcHist([img], [0], None, [256], [0, 256])
@@ -22,18 +21,6 @@ plt.figure()
 plt.plot(gray_img_2)
 # プロットの保存
 plt.savefig('result/gray_img_2.png')
-=======
-# colorのタプル
-color = ('b', 'g', 'r')
-
-for i, col in enumerate(color):
-    # colorのインデックスとbgrを格納
-    histr = cv2.calcHist([img], [i], None, [256], [0, 256])
-    # ヒストグラムをプロット
-    plt.plot(histr, color=col)
-    # X軸を0-256の間でヒストグラム表を作る
-    plt.xlim([0, 256])
->>>>>>> 5a79a2156ac0bcc76ad3d930999be163969c05a0
 
 # プロットの表示
 plt.show()
