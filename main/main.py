46fdf2cdee3bff77891762e09647217080b629db
bg823 <nagaoka0132@gmail.com>
2020-04-18 17:17:22 +0900

    create RGB to GRAY movie.

--- main/main.py
+++ main/main.py
@@ -7,36 +7,38 @@ import cv2
 # ビデオキャプチャ生成、引数はカメラデバイスの番号
 # 自分がキャプチャしたい、ビデオ番号を指定
 # PCのデフォルトを使う場合は、1 or 0 or -1 その他の可能性もあり
-cap = cv2.VideoCapture(0)
+cap1 = cv2.VideoCapture(
+    '/your file path/fortnite.MOV')
 
 # cap.isOpen()でキャプチャが生成されているか確認することができる
 # return パラメーターは　True or False
-while (cap.isOpened()):
+while (cap1.isOpened()):
     # retには、読み込めているかどうかのbool値、frameには1フレームが入ってくる
-    ret, frame = cap.read()
+    ret1, frame1 = cap1.read()
 
     # retがTrueじゃなければ、break
-    if not ret:
+    if not ret1:
         break
 
     # カラーフレームをグレーフレームに変換(RGB->GRAY)
     # cvtColor(input_img, flg)
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    gray = cv2.cvtColor(frame1, cv2.COLOR_RGB2GRAY)
 
     # window sizeをdefaultから、X=320, Y=240に変更
     gray = cv2.resize(gray, dsize=(320, 240))
+
     # グレー変換した、フレームをcaputreという名前のWindowで表示
-    cv2.imshow('capture', gray)
+    cv2.imshow('capture1', gray)
 
     # escキーが押された場合、ブレークしてwhileを抜ける
     # 0にすると、キーを受け付けるまで、待機してしまうため、フレームが更新されない
     # そのため、適切な数値を振る必要がある
-    # 今回の場合、待機は1msecになる
-    if cv2.waitKey(1) & 0xFF == 27:
+    # 今回の場合は、
+    if cv2.waitKey(30) & 0xFF == 27:
         break
 
 # ビデオファイルまたは、キャプチャデバイスを閉じる
-cap.release()
+cap1.release()
 
 # 表示している画面を破棄
 cv2.destroyAllWindows()
