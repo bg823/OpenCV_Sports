1605d8f7f51e2901db1851bbecdda8bf3ab7e60c
bg823 <nagaoka0132@gmail.com>
2020-04-22 21:46:30 +0900

    Draw 3 color circles on the image.

--- main/main.py
+++ main/main.py
@@ -9,12 +9,12 @@ img = cv2.imread('/Users/h-nagaoka/Dev/OpenCV_Sports/images/bg823.png')
 if img is None:
     sys.exit()
 
-# create 3 line colors on read image
-# writing line is Coordinate X50Y50-X200-Y50 and X50Y100-X200Y100 and X100Y150-X200Y150
-# thickness 5 and 10px
-cv2.line(img, (50, 50), (200, 50), (255, 0, 0))
-cv2.line(img, (50, 100), (200, 100), (0, 255, 0), 5)
-cv2.line(img, (100, 150), (200, 150), (0, 0, 255), 10)
+
+# writing circle on image
+# color is green and skyblue? and yellow
+cv2.circle(img, (50, 50), 40, (0, 255, 0), 2)
+cv2.circle(img, (150, 150), 80, (255, 255, 0), 6)
+cv2.circle(img, (200, 200), 50, (0, 255, 255), -1)
 
 # save image and show
 cv2.imwrite('result/line.png', img)
