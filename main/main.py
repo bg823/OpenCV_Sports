bbe9f6a7140c4438673cb23bcfa23c7ab6f7651f
bg823 <nagaoka0132@gmail.com>
2020-04-22 21:16:26 +0900

    write circle image and save.

--- main/main.py
+++ main/main.py
@@ -5,34 +5,40 @@ import cv2  # import opencv
 
 # initialize with 0.
 # create Three-dimensional array is type uint8
-# image size 400 * 400 and three colors(channel)
-# np.zeroz((line, row, color_channel))
-img = np.zeros((400, 400, 3), np.uint8)
+img1 = np.zeros((400, 400, 3), np.uint8)
 
-# create blue image
-img[:, :] = [255, 0, 0]
-# writing blue image in main folder
-cv2.imwrite('result/blueImage.jpg', img)
-# show image on window
-cv2.imshow('img1', img)
+# create background black in blue line circle image
+# from center 200px, 200px and  50px radius, 1px line
+cv2.circle(img1, (200, 200), 50, (255, 0, 0), 1)
 
-# create green image
-img[:, :] = [0, 255, 0]
-# writing blue image in main folder
-cv2.imwrite('result/greenImage.jpg', img)
-# show image on window
-cv2.imshow('img2', img)
+# save circle image and show
+cv2.imwrite('result/circle1.png', img1)
+cv2.imshow('circle1', img1)
 
-# create red image
-img[:, :] = [0, 0, 255]
-# writing blue image in main folder
-cv2.imwrite('result/redImage.jpg', img)
-# show image on window
-cv2.imshow('img3', img)
+# initialize with 0.
+# create Three-dimensional array is type uint8
+img2 = np.zeros((400, 400, 3), np.uint8)
 
-while True:
-    # push esc kye is exit
-    if cv2.waitKey(0) & 0xff == 27:
-        # closing all Window
-        cv2.destroyAllWindows()
-        break
+# create background black in green line circle image
+# from center 200px, 200px and  100px radius, 3px line
+cv2.circle(img2, (200, 200), 100, (0, 255, 0), 3)
+
+# save circle image and show
+cv2.imwrite('result/circle2.png', img2)
+cv2.imshow('circle2', img2)
+
+# initialize with 0.
+# create Three-dimensional array is type uint8
+img3 = np.zeros((400, 400, 3), np.uint8)
+
+# create background black in green line circle image
+# from center 200px, 200px and  50px radius, 1px line
+cv2.circle(img3, (200, 200), 150, (0, 0, 255), -1)
+
+# save circle image and show
+cv2.imwrite('result/circle3.png', img3)
+cv2.imshow('circle3', img3)
+
+# end when key is pressed and close windows
+cv2.waitKey(0)
+cv2.destroyAllWindows()
