cee0cd9358f6ea6c9b1072ce12359892f0809bfb
bg823 <nagaoka0132@gmail.com>
2020-05-05 12:46:17 +0900

    drowing polylines. and add images.

--- main/main.py
+++ main/main.py
@@ -9,13 +9,16 @@ import numpy as np
 # 真っ黒なイメージを作成 width height共に512PX
 img = np.zeros((512, 512, 3), np.uint8)
 
-# 白で指定したテキストを描く
-# cv2.putText(書き込むイメージ, 書きたいテキスト, テキストの左端, フォント, フォントサイズ, 色, 太さ)
-font = cv2.FONT_HERSHEY_SIMPLEX
-cv2.putText(img, 'OpenCV', (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
+# polylineを描画するための配列を作成
+pts = np.array([[10, 5], [20, 30], [70, 20], [50, 10]], np.int32)
+pts = pts.reshape((-1, 1, 2))
+
+# 黄色の多角線を描く
+# cv2.polylines(書き込むイメージ, 多角線の配列, 多角線が閉じているかTrue False, 色, エッジの太さ)
+cv2.polylines(img, [pts], True, (0, 255, 255))
 
 # 画像の表示
-cv2.imshow('putText windwow', img)
+cv2.imshow('polylines windwow', img)
 
 # 何かしらのキーが押されたら、表示しているwindowを破棄する
 cv2.waitKey(0)
