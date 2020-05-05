d84a23973208a3ec5825ec09403e8a955986b6b5
bg823 <nagaoka0132@gmail.com>
2020-05-05 10:50:11 +0900

    drowing putText is 'OpenCV'. add save images

--- main/main.py
+++ main/main.py
@@ -9,12 +9,13 @@ import numpy as np
 # 真っ黒なイメージを作成 width height共に512PX
 img = np.zeros((512, 512, 3), np.uint8)
 
-# 青の楕円を描く
-# cv2.ellipse(書き込むイメージ,　楕円の中心, 幅, 楕円回転角度, 楕円弧の開始角度, 楕円弧の終了角度, 色, 太さ)
-cv2.ellipse(img, (256, 256), (100, 50), 0, 0, 180, (255, 0, 0), -1)
+# 白で指定したテキストを描く
+# cv2.putText(書き込むイメージ, 書きたいテキスト, テキストの左端, フォント, フォントサイズ, 色, 太さ)
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img, 'OpenCV', (10, 500), font, 4, (255, 255, 255), 2, cv2.LINE_AA)
 
 # 画像の表示
-cv2.imshow('ellipse windwow', img)
+cv2.imshow('putText windwow', img)
 
 # 何かしらのキーが押されたら、表示しているwindowを破棄する
 cv2.waitKey(0)
