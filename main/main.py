b1df87723e9f07b17b616080731b2778ae01195b
bg823 <nagaoka0132@gmail.com>
2020-05-16 11:59:19 +0900

    add confirm setMouseCallback() function events and flags.

--- main/main.py
+++ main/main.py
@@ -1,25 +1,9 @@
 """
-Drowing Functions in OpenCV
-reference site : https://docs.opencv.org/4.2.0/dc/da5/tutorial_py_drawing_functions.html
+Mouse as a Paint-Brush in OpenCV
+reference site : https://docs.opencv.org/4.2.0/db/d5b/tutorial_py_mouse_handling.html
 """
-import cv2
-import numpy as np
+import cv2 as cv
+import pprint
 
-
-# 真っ黒なイメージを作成 width height共に512PX
-img = np.zeros((512, 512, 3), np.uint8)
-
-# polylineを描画するための配列を作成
-pts = np.array([[10, 5], [20, 30], [70, 20], [50, 10]], np.int32)
-pts = pts.reshape((-1, 1, 2))
-
-# 黄色の多角線を描く
-# cv2.polylines(書き込むイメージ, 多角線の配列, 多角線が閉じているかTrue False, 色, エッジの太さ)
-cv2.polylines(img, [pts], True, (0, 255, 255))
-
-# 画像の表示
-cv2.imshow('polylines windwow', img)
-
-# 何かしらのキーが押されたら、表示しているwindowを破棄する
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+events = [i for i in dir(cv) if 'EVENT' in i]
+pprint.pprint(events)
