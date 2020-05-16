cf59819d2ff1fc402517c131de32e22c5e5fe511
bg823 <nagaoka0132@gmail.com>
2020-05-16 18:25:10 +0900

    add advanced setMouseCallback functions. drowing rectangle, circles.

--- main/main.py
+++ main/main.py
@@ -5,12 +5,39 @@ reference site : https://docs.opencv.org/4.2.0/db/d5b/tutorial_py_mouse_handling
 import cv2
 import numpy as np
 
+drowing = False
+mode = True
+ix, iy = -1, -1
+
 
 # マウスコールバック関数
 def drow_circle(event, x, y, flags, param):
-    # マウスの左ボタンがダブルクリックされたら、XY座標にクリックした座標を当てて塗り潰した円を描く
-    if event == cv2.EVENT_LBUTTONDBLCLK:
-        cv2.circle(img, (x, y), 100, (255, 0, 0), -1)
+    global ix, iy, drowing, mode
+
+    # print(mode)
+
+    # マウスの左ボタンが押された場合
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drowing = True
+        ix, iy = x, y
+    # マウスポインタがウィンドウ上に移動した
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drowing == True:
+            if mode == True:
+                # 四角を描画する
+                cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
+            else:
+                # 円を描画する
+                cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+    # マウスの左ボタンが離された
+    elif event == cv2.EVENT_LBUTTONUP:
+        drowing = False
+        if mode == True:
+            # 四角を描画する
+            cv2.rectangle(img, (ix, iy), (x, y), (0, 255, 0), -1)
+        else:
+            # 円を描画する
+            cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
 
 
 # イメージの作成とwindowの作成、setMouseCallbackにバインド
@@ -21,7 +48,12 @@ cv2.setMouseCallback('img', drow_circle)
 
 while (1):
     cv2.imshow('img', img)
-    if cv2.waitKey(20) & 0xFF == 27:
+    # Tabが押されたら、modeをFalseに設定
+    key = cv2.waitKey(1) & 0xFF
+    if key == 9:
+        mode = not mode
+        # escが押されたら、window破棄
+    elif key == 27:
         break
 
 cv2.destroyAllWindows()
